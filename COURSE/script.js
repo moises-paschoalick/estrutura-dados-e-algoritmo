436592b6c29ff8a1df1d508b2627599b95ccb61c
Moisés Guilherme <moisescomp@gmail.com>
2025-03-21 07:39:42 -0300

    Funções em Javascript sem OO

--- COURSE/script.js
+++ COURSE/script.js
@@ -1,5 +1,23 @@
-class Product {
+function Product(name, price, quantity)  {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
 
+    this.toString = function() {
+        return `${this.name}, $${this.price.toFixed(2)}, qty: ${this.quantity}`;
+    }
+}
+
+function total(product) {
+    return product.price * product.quantity;
+}
+
+function updatePrice(product, percentage) {
+    product.price = product.price * (1 + percentage/100)
+}
+
+ // Sintaxe sugar (alternativa q o JS entrega)
+/*class Product {   
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
@@ -12,16 +30,17 @@ class Product {
     }
 
 
-}
+}*/
 
 const p1 = new Product("Laptop", 1000.0, 5);
 const p2 = new Product("Headphones", 200.0, 2);
 
-console.log(p1.name);
-console.log(p1.price)
-console.log(p1.quantity)
+const total1 = total(p1);
+const total2 = total(p2);
 
-console.log(p2.name);
+console.log(total1);
+console.log(total2);
 
-console.log(p1.toString())
-console.log(p2.toString())
+updatePrice(p1, 10);
+
+console.log(p1.price);
