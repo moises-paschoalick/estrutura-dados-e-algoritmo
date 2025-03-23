61cb78a43920da8c0c863cd64d39b1f7bcf9aaff
Moisés Guilherme <moisescomp@gmail.com>
2025-03-22 21:44:09 -0300

    Funções em Java com OO

--- COURSE/JAVA/course/src/app/Product.java
+++ COURSE/JAVA/course/src/app/Product.java
@@ -40,6 +40,15 @@ public class Product {
         this.quantity = quantity;
     }
 
+    // Com orientação objetos
+    public double total() {
+        return price * quantity;
+    }
+
+    public void updatePrice(double percentage) {
+        price = price * (1.0 + (percentage/100.0));
+    }
+
     @Override
     public String toString() {
         return name + ", $" + String.format("%.2f", price) + ", " + quantity;
--- COURSE/JAVA/course/src/app/Program.java
+++ COURSE/JAVA/course/src/app/Program.java
@@ -2,15 +2,6 @@ package app;
 
 public class Program {
 
-    // Sem orientação objetos
-    static double total(Product product) {
-        return product.getPrice() * product.getQuantity();
-    }
-
-    static void updatePrice(Product product, double percentage) {
-        var newPrice = product.getPrice() * (1.0 + (percentage/100.0));
-        product.setPrice(newPrice);
-    }
 
     public static void main(String[] args) {
 
@@ -18,13 +9,16 @@ public class Program {
         var p1 = new Product("Laptop", 1000.0, 5);
         var p2 = new Product("Headphones", 200.0, 2); //200.0 double
 
-        var total1 = total(p1);
-        var total2 = total(p2);
+        var total1 = p1.total();
+        var total2 = p2.total();
 
         System.out.println(total1);
         System.out.println(total2);
 
-        updatePrice(p1, 20.0);
+        p1.updatePrice(20.0);
+
+        //Product.updatePrice(p1, 20);
+
         System.out.println(p1.getPrice());
     }
 }
