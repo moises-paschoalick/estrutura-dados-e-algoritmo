0bb461fb3e0fb0952dcbe38dfb9b737e0a7e319d
Moisés Guilherme <moisescomp@gmail.com>
2025-03-24 20:44:15 -0300

    Criacao projeto Java, mapeamento entidades

---
+++ COURSE/JAVA/course/read-json/readJson/src/main/java/entities/Lesson.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.time.Instant;
+
+public class Lesson {
+
+    private Long id;
+    private String title;
+    private String media;
+    private Instant timestamp;
+
+    public Lesson() {
+    }
+
+    public Lesson(Long id, String title, String media) {
+        this.id = id;
+        this.title = title;
+        this.media = media;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+}
