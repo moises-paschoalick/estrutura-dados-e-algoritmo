184b8f8cba8486b7ea3da012ffe82298b32c83d5
Moisés Guilherme <moisescomp@gmail.com>
2025-03-26 07:33:56 -0300

    Literais e expressões em Javascript PARTE 1

---
+++ COURSE/JS/STRING/demo-literals.js
@@ -0,0 +1,17 @@
+const name = "Alice"; //Imutável
+const age = 31;
+const points = 8.347;
+
+// Simple literal
+const str1 = "Olá, mundo!";
+const str1_2 = 'Olá mundo!';
+const str1_3 = `Olá mundo!`; //Crase (template literal)
+
+console.log("str1:" );
+console.log(str1);
+
+// Concatenação
+const str2 = "Cliente " + name + " tem idade " + age + " anos e fez " + points + " pontos.";
+console.log("str2:");
+console.log(str2);
+
