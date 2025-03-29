79f81e3c5d4c98068519ce70a4237d577c016d0b
Moisés Guilherme <moisescomp@gmail.com>
2025-03-29 18:03:38 -0300

    Exemplo limpar CPF

--- COURSE/JS/STRING/demo-regex.js
+++ COURSE/JS/STRING/demo-regex.js
@@ -6,3 +6,11 @@ function validateCEP(cep) {
 console.log(validateCEP('12345-678'));
 console.log(validateCEP('12345678'));
 console.log(validateCEP('1234-5678'));
+
+function removeNonDigits(string) {
+    const regex = /\D/g;
+    return string.replace(regex, "");
+}
+
+console.log(removeNonDigits("94923784799"));
+console.log(removeNonDigits("213.445.034-82"));
